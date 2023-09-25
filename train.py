c592eb433eee6542283a7ecf95176d29a4120aa5
WuShaogui <wshglearn@163.com>
2023-09-25 15:30:39 +0800

    丢弃pytorch-lighting，手动训练

--- train.py
+++ train.py
@@ -1,58 +1,151 @@
+import datetime
 import os
-import yaml
-import argparse
-import numpy as np
-from pathlib import Path
-import torch.backends.cudnn as cudnn
-from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import TensorBoardLogger
-from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
-
+import torch
+from torch.utils.data import DataLoader
 from data.dataset import VAEDataset
 from nets.vanilla_vae import VanillaVAE
-from train.trainer import VAEXperiment
+import torchvision.utils as vutils
+import numpy as np
 
+import sys
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generic runner for VAE models')
-    parser.add_argument('--config',  '-c',
-                        dest="filename",
-                        metavar='FILE',
-                        help =  'path to the config file',
-                        default='train/vae.yaml')
+if sys.gettrace() is not None:
+    # Code 1: This code will be executed when debugging
+    print("Debugging")
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+else:
+    # Code 2: This code will be executed when running without debugging
+    print("Running")
+    os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
-    args = parser.parse_args()
-    with open(args.filename, 'r') as file:
-        try:
-            config = yaml.safe_load(file)
-        except yaml.YAMLError as exc:
-            print(exc)
+if __name__ == "__main__":
+    # TODO 加载数据集
+    data_path = "/home/wushaogui/MyCodes/Pytorch_VAE/imgs/中航阳极分类数据"
+    train_label_lines = [
+        image_path.replace("\n", "")
+        for image_path in open(
+            os.path.join(data_path, "train.txt"), encoding="utf-8", mode="r"
+        ).readlines()
+    ]
+    val_label_lines = [
+        image_path.replace("\n", "")
+        for image_path in open(
+            os.path.join(data_path, "val.txt"), encoding="utf-8", mode="r"
+        ).readlines()
+    ]
+    input_shape = (64, 64)
+    batch_size = 16
+    train_dataset = VAEDataset(train_label_lines, input_shape)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True, pin_memory=True
+    )
+    val_dataset = VAEDataset(train_label_lines, input_shape)
+    val_dataloader = DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, pin_memory=True
+    )
 
-    data = VAEDataset(**config["data_params"], pin_memory=len(config['trainer_params']['gpus']) != 0)
-    data.setup()
+    # TODO 加载模型
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    in_channels = 3
+    latent_dim = 256
+    net = VanillaVAE(device, in_channels, latent_dim)
+    net = net.to(device)
 
-    tb_logger =  TensorBoardLogger(save_dir=config['logging_params']['save_dir'],
-                                name=config['model_params']['name'],)
-    
-    in_channels=3
-    latent_dim=128
-    model = VanillaVAE(in_channels, latent_dim)
-    experiment = VAEXperiment(model,
-                            config['exp_params'])
-    
-    runner = Trainer(logger=tb_logger,
-                    callbacks=[
-                        LearningRateMonitor(),
-                        ModelCheckpoint(save_top_k=2, 
-                                        dirpath =os.path.join(tb_logger.log_dir , "checkpoints"), 
-                                        monitor= "val_loss",
-                                        save_last= True),
-                    ])
+    # 定义优化函数
+    opt = torch.optim.Adam(net.parameters(), lr=1e-3, weight_decay=5e-4)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(opt, step_size=3, gamma=0.98)
 
+    # 训练模型
+    log_subdir = datetime.datetime.strftime(
+        datetime.datetime.now(), "%Y_%m_%d_%H_%M_%S"
+    )
+    log_dir = os.path.abspath(os.path.join("runs", log_subdir))
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-    Path(f"{tb_logger.log_dir}/Samples").mkdir(exist_ok=True, parents=True)
-    Path(f"{tb_logger.log_dir}/Reconstructions").mkdir(exist_ok=True, parents=True)
+    min_loss = np.inf
+    num_epoches = 30
+    for epoch in range(num_epoches):
+        train_total_loss = 0
+        net = net.train()
+        for i, batch in enumerate(train_dataloader):
+            img = batch
+            img = img.to(device)
+            # 前向推理
+            reconstructed_x, input, mu, log_var = net(img)
+            loss = net.loss_function(reconstructed_x, input, mu, log_var, M_N=0.00025)
+            train_loss = loss["loss"]
 
+            # 后向更新
+            opt.zero_grad()
+            train_loss.backward()
+            opt.step()
 
-    print(f"======= Training {config['model_params']['name']} =======")
-    runner.fit(experiment, datamodule=data)
+            # 统计损失
+            reconstruction_loss = loss["Reconstruction_Loss"]
+            kld_loss = loss["KLD"]
+            train_total_loss += train_loss.item()
+
+            if i % 10 == 0:
+                print(
+                    "epoch:{} train loss:{:.4f} reconstruction_loss:{:.4f} kld_loss:{:.4f}".format(
+                        epoch,
+                        train_total_loss / (i + 1),
+                        reconstruction_loss.item(),
+                        kld_loss.item(),
+                    )
+                )
+
+        # 评估阶段
+        net = net.eval()
+        val_total_loss = 0
+        for i, batch in enumerate(val_dataloader):
+            img = batch
+            img = img.to(device)
+            results = net(img)
+            val_loss = net.loss_function(
+                *results,
+                M_N=1.0,
+            )
+            val_total_loss += val_loss["loss"].item()
+            reconstruction_loss = val_loss["Reconstruction_Loss"]
+            kld_loss = val_loss["KLD"]
+
+            if i % 10 == 0:
+                print(
+                    "epoch:{} val loss:{:.4f} reconstruction_loss:{:.4f} kld_loss:{:.4f}".format(
+                        epoch,
+                        val_total_loss / (i + 1),
+                        reconstruction_loss.item(),
+                        kld_loss.item(),
+                    )
+                )
+        if val_total_loss / (i + 1) < min_loss:
+            torch.save(net.eval().state_dict(), os.path.join("runs/best.pth"))
+
+        # 重建样本阶段
+        if not os.path.exists(os.path.join(log_dir, "Reconstructed")):
+            os.makedirs(os.path.join(log_dir, "Reconstructed"))
+        test_input = next(iter(val_dataloader))
+        test_input = test_input.to(device)
+        recons = net.generate(test_input)
+        vutils.save_image(
+            recons.data,
+            "{}/Reconstructed/epoch-{}.png".format(log_dir, epoch),
+            normalize=True,
+            nrow=4,
+        )
+
+        # 随机生成样本阶段
+        if not os.path.exists(os.path.join(log_dir, "Samples")):
+            os.makedirs(os.path.join(log_dir, "Samples"))
+        samples = net.sample(16, device)
+        vutils.save_image(
+            samples.cpu().data,
+            "{}/Samples/epoch-{}.png".format(log_dir, epoch),
+            normalize=True,
+            nrow=4,
+        )
+
+        # 学习率衰减
+        lr_scheduler.step()
