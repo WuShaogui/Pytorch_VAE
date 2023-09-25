9fad4242a670f0c306950856dd85c865b934ca1f
WuShaogui <wshglearn@163.com>
2023-09-25 16:47:07 +0800

    增加模型初始化方法

--- nets/vanilla_vae.py
+++ nets/vanilla_vae.py
@@ -6,12 +6,42 @@ from typing import Any, List
 from nets.base import BaseVAE
 
 
+def weights_init(net, init_type="normal", init_gain=0.02):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, "weight") and classname.find("Conv") != -1:
+            if init_type == "normal":
+                torch.nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == "xavier":
+                torch.nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == "kaiming":
+                torch.nn.init.kaiming_normal_(m.weight.data, a=0, mode="fan_in")
+            elif init_type == "orthogonal":
+                torch.nn.init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError(
+                    "initialization method [%s] is not implemented" % init_type
+                )
+        elif classname.find("BatchNorm2d") != -1:
+            torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+            torch.nn.init.constant_(m.bias.data, 0.0)
+
+    print("initialize network with %s type" % init_type)
+    net.apply(init_func)
+
+
 class VanillaVAE(BaseVAE):
     def __init__(
-        self, in_channels: int, latent_dim: int, hidden_dims: List = None, **kwargs
+        self,
+        device,
+        in_channels: int,
+        latent_dim: int,
+        hidden_dims: List = None,
+        **kwargs
     ) -> None:
         super(VanillaVAE, self).__init__()
 
+        self.device = device
         self.latent_dim = latent_dim
 
         modules = []
@@ -36,14 +66,14 @@ class VanillaVAE(BaseVAE):
             in_channels = h_dim
 
         self.encoder = nn.Sequential(*modules)
-        
-        self.fc_mu = nn.Linear(hidden_dims[-1] * 4, latent_dim)
-        self.fc_var = nn.Linear(hidden_dims[-1] * 4, latent_dim)
+
+        self.fc_mu = nn.Linear(hidden_dims[-1] * 16, latent_dim)
+        self.fc_var = nn.Linear(hidden_dims[-1] * 16, latent_dim)
 
         # Build Decoder
         modules = []
 
-        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 4)
+        self.decoder_input = nn.Linear(latent_dim, hidden_dims[-1] * 16)
 
         hidden_dims.reverse()
 
@@ -58,6 +88,14 @@ class VanillaVAE(BaseVAE):
                         padding=1,
                         output_padding=1,
                     ),
+                    # nn.UpsamplingBilinear2d(scale_factor=2),
+                    # nn.Conv2d(
+                    #     hidden_dims[i],
+                    #     out_channels=hidden_dims[i + 1],
+                    #     kernel_size=3,
+                    #     stride=1,
+                    #     padding=1,
+                    # ),
                     nn.BatchNorm2d(hidden_dims[i + 1]),
                     nn.LeakyReLU(),
                 )
@@ -80,7 +118,7 @@ class VanillaVAE(BaseVAE):
             nn.Tanh(),
         )
 
-    def encode(self, input: Tensor) -> List[Tensor]:
+    def encode(self, input):
         """
         Encodes the input by passing through the encoder network
         and returns the latent codes.
@@ -97,7 +135,7 @@ class VanillaVAE(BaseVAE):
 
         return [mu, log_var]
 
-    def decode(self, z: Tensor) -> Tensor:
+    def decode(self, z):
         """
         Maps the given latent codes
         onto the image space.
@@ -105,12 +143,12 @@ class VanillaVAE(BaseVAE):
         :return: (Tensor) [B x C x H x W]
         """
         result = self.decoder_input(z)
-        result = result.view(-1, 512, 2, 2)
+        result = result.view(-1, 512, 4, 4)
         result = self.decoder(result)
         result = self.final_layer(result)
         return result
 
-    def reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
+    def reparameterize(self, mu, logvar):
         """
         Reparameterization trick to sample from N(mu, var) from
         N(0,1).
@@ -118,16 +156,16 @@ class VanillaVAE(BaseVAE):
         :param logvar: (Tensor) Standard deviation of the latent Gaussian [B x D]
         :return: (Tensor) [B x D]
         """
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
+        std = torch.exp(0.5 * logvar).to(self.device)
+        eps = torch.randn_like(std).to(self.device)
         return eps * std + mu
 
-    def forward(self, input: Tensor, **kwargs) -> List[Tensor]:
-        mu, log_var = self.encode(input)     # torch.Size([2, 128]) torch.Size([2, 128])
-        z = self.reparameterize(mu, log_var) # torch.Size([2, 128])
+    def forward(self, input, **kwargs):
+        mu, log_var = self.encode(input)  # torch.Size([2, 128]) torch.Size([2, 128])
+        z = self.reparameterize(mu, log_var)  # torch.Size([2, 128])
         return [self.decode(z), input, mu, log_var]
 
-    def loss_function(self, *args, **kwargs) -> dict:
+    def loss_function(self, *args, **kwargs):
         """
         Computes the VAE loss function.
         KL(N(\mu, \sigma), N(0, 1)) = \log \frac{1}{\sigma} + \frac{\sigma^2 + \mu^2}{2} - \frac{1}{2}
@@ -154,7 +192,7 @@ class VanillaVAE(BaseVAE):
             "KLD": -kld_loss.detach(),
         }
 
-    def sample(self, num_samples: int, current_device: int, **kwargs) -> Tensor:
+    def sample(self, num_samples: int, current_device: int, **kwargs):
         """
         Samples from the latent space and return the corresponding
         image space map.
@@ -169,7 +207,7 @@ class VanillaVAE(BaseVAE):
         samples = self.decode(z)
         return samples
 
-    def generate(self, x: Tensor, **kwargs) -> Tensor:
+    def generate(self, x, **kwargs):
         """
         Given an input image x, returns the reconstructed image
         :param x: (Tensor) [B x C x H x W]
@@ -182,20 +220,21 @@ class VanillaVAE(BaseVAE):
 if __name__ == "__main__":
     in_channels = 3
     latent_dim = 128
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    print(device)
 
     # 模拟训练过程
-    inputs = torch.rand((2, 3, 64, 64))
-    net = VanillaVAE(in_channels, latent_dim)
-    reconstructed_x,input, mu, log_var = net(inputs)
-    print(reconstructed_x.shape,mu.shape, log_var.shape)
+    inputs = torch.rand((2, 3, 128, 128)).to(device)
+    net = VanillaVAE(device, in_channels, latent_dim)
+    net = net.to(device)
+    reconstructed_x, input, mu, log_var = net(inputs)
+    print(reconstructed_x.shape, mu.shape, log_var.shape)
 
     # 模拟生成样本过程
     num_samples = 3
-    current_device=torch.device('cpu')
-    samples=net.generate(num_samples,current_device)
+    samples = net.sample(num_samples, device)
     print(samples.shape)
-    
+
     # 模拟计算损失函数
-    loss=net.loss_function(reconstructed_x,input, mu, log_var)
+    loss = net.loss_function(reconstructed_x, input, mu, log_var, M_N=0.00025)
     print(loss)
-    
